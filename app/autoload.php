7d077fe4984b824c58c39da1b862dfd990cc8fbe
Simon Malpel (SimonOriginal) <simon.malpel@orange.fr>
2019-09-13 16:39:41 +0200

    Add config file (Fix.)

--- app/autoload.php
+++ app/autoload.php
@@ -4,6 +4,6 @@
     require __DIR__ . '/class/autoloader.php'; 
     Autoloader::register();
 
-    $PDO = function($host, $database, $charset, $username, $password = null, $options = null) { return new \PDO("mysql:host=$host;dbname=$database;charset=$charset", $username, $password, $options); };
+    $PDO = function() use ($Config) { return new \PDO('mysql:host=' . $Config['host'] . ';dbname=' . $Config['database'] . ';charset=' . $Config['charset'] . '', $Config['username'], $Config['password'], $Config['options']); };
 
 ?>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
     require realpath(__DIR__ . '/..') . '/app/autoload.php';
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     if (!empty($_GET['token']) && !empty($_GET['request'])) {
         $token      = $_GET['token'];
         $request    = $_GET['request'];
